fdb48dbe5bf26ba7ca37688ca930b55a9b7060b2
carolinelee <leesx92@gmail.com>
2023-04-23 23:06:14 +0800

    Added new Spring Security configuration and update some Account features

---
+++ src/main/java/com/company/payroll/configuration/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.company.payroll.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+//@Configuration
+//@EnableWebSecurity
+public class WebSecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((request) -> 
+			request.requestMatchers("/**").permitAll()
+				
+			);
+		
+		return http.build();
+		
+	}
+	
+//	@Bean
+//	public UserDetailsService userDetailsService() {
+//		UserDetails user = User.withUsername("administrator")
+//							.password("{bcrypt}$2a$10$LyyWPUq/G7ZnSj8rWuzr9uyplAglRCazGzPkRQd8tn3WgJ5lcfvze")
+//							.roles("USER")
+//							.build();
+//		
+//		return new InMemoryUserDetailsManager(user);
+		
+//	}
+}
