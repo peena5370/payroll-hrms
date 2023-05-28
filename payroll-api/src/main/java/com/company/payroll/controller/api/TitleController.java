e8e117d3fc00f5d8113216a7da83d6324bce9df2
carolinelee <leesx92@gmail.com>
2023-05-28 22:42:14 +0800

    modify spacing

--- payroll-api/src/main/java/com/company/payroll/controller/api/TitleController.java
+++ payroll-api/src/main/java/com/company/payroll/controller/api/TitleController.java
@@ -36,8 +36,7 @@ public class TitleController {
 
 	@Operation(summary="Get title list")
 	@GetMapping
-	public ResponseEntity<PageInfo<Title>> listTitleByPage(@RequestParam(value="page", required=true) int page, 
-														  @RequestParam(value="size", required=true) int offset) {
+	public ResponseEntity<PageInfo<Title>> listTitleByPage(@RequestParam(value="page", required=true) int page, @RequestParam(value="size", required=true) int offset) {
 		return ResponseEntity.ok(titleService.getListByPage(page, offset));
 	}
 
@@ -48,12 +47,12 @@ public class TitleController {
 	}
 	
 	@Operation(summary="Insert title info",
-			   responses= {@ApiResponse(responseCode="200",
-					   					description="Value return 1 for insert success.",
-					   					content=@Content(examples= {@ExampleObject(value="1")})),
-					   	   @ApiResponse(responseCode="403",
-					   			   		description="Value return 0 for insert fail.",
-					   			   		content=@Content(examples= {@ExampleObject(value="0")}))})
+		responses= {@ApiResponse(responseCode="200",
+				description="Value return 1 for insert success.",
+				content=@Content(examples= {@ExampleObject(value="1")})),
+				@ApiResponse(responseCode="403",
+				description="Value return 0 for insert fail.",
+				content=@Content(examples= {@ExampleObject(value="0")}))})
 	@io.swagger.v3.oas.annotations.parameters.RequestBody(
 			   	 content= {@Content(mediaType="application/json", 
 	   			 schema= @Schema(implementation = Title.class),
@@ -66,12 +65,12 @@ public class TitleController {
 	}
 	
 	@Operation(summary="Update title info.",
-			   responses= {@ApiResponse(responseCode="200",
-										description="Value return 1 for update success.",
-										content=@Content(examples= {@ExampleObject(value="1")})),
-					   	  @ApiResponse(responseCode="403",
-					   	  				description="Value return 0 for update fail.",
-					   	  				content=@Content(examples= {@ExampleObject(value="0")}))})
+		responses= {@ApiResponse(responseCode="200",
+				description="Value return 1 for update success.",
+				content=@Content(examples= {@ExampleObject(value="1")})),
+				@ApiResponse(responseCode="403",
+				description="Value return 0 for update fail.",
+				content=@Content(examples= {@ExampleObject(value="0")}))})
 	@PutMapping("/{id}")
 	public ResponseEntity<Title> update(@RequestBody Title title) {
 		Title status = titleService.update(title);
@@ -80,12 +79,12 @@ public class TitleController {
 	}
 	
 	@Operation(summary="Delete title info.",
-			   responses= {@ApiResponse(responseCode="200",
-										description="Value return 1 for delete success.",
-										content=@Content(examples= {@ExampleObject(value="1")})),
-					   	  @ApiResponse(responseCode="403",
-					   	  				description="Value return 0 for delete fail.",
-					   	  				content=@Content(examples= {@ExampleObject(value="0")}))})
+		responses= {@ApiResponse(responseCode="200",
+				description="Value return 1 for delete success.",
+				content=@Content(examples= {@ExampleObject(value="1")})),
+				@ApiResponse(responseCode="403",
+				description="Value return 0 for delete fail.",
+				content=@Content(examples= {@ExampleObject(value="0")}))})
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Integer> delete(@Parameter() @PathVariable("id") int titleno) {
 		Integer status = titleService.delete(titleno);
